1fe77c6cdf49ed676fce3453ae56fe1a399b7489
Lucas Cavalheiro <lucasribeiro120@hotmail.com>
2022-05-21 19:56:47 -0300

    Criando telas do Funcionario e a tela Home de periodico

---
+++ Client/Pages/funcionario/FuncionarioViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Projeto_Contabilidade.Client.Pages.funcionario
+{
+    public class FuncionarioViewModel
+    {
+
+        #region Pessoal
+        public string CodFuncionario { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Cargo { get; set; }
+        public string Email { get; set; }
+        public string DataNascimento { get; set; }
+        #endregion
+
+        #region Endereço
+        public string Rua { get; set; }
+        public string Numero { get; set; }
+        public string Bairro { get; set; }
+        public string NomeCidade { get; set; }
+        #endregion
+    }
+}
---
+++ Client/Pages/periodico/PeriodicoViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Projeto_Contabilidade.Client.Pages.periodico
+{
+    public class PeriodicoViewModel 
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
