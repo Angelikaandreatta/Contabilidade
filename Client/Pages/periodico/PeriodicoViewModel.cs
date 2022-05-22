66078f7a3f81692e58be9f4789b672bd17d749f5
Lucas Cavalheiro <lucasribeiro120@hotmail.com>
2022-05-22 01:11:50 -0300

    Mudando os campos do cadastro de periodico

--- Client/Pages/periodico/PeriodicoViewModel.cs
+++ Client/Pages/periodico/PeriodicoViewModel.cs
@@ -4,5 +4,7 @@
     {
         public string Codigo { get; set; }
         public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Categoria { get; set; }
     }
 }
