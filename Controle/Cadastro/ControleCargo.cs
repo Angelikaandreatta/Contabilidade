0d1a8f477a8974c4f3113dc08bb1cbff7236e7f1
Vinicius Chapula <viniciuschapula@gmail.com>
2022-06-06 12:07:05 -0300

    Criando o controle e a controller do cargo com os metodos carregar e listar

---
+++ Controle/Cadastro/ControleCargo.cs
@@ -0,0 +1,74 @@
+﻿using BancoDeDados.Cadastro;
+using Negocio.Cadastro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle.Cadastro
+{
+    public class ControleCargo
+    {
+        public async Task<List<Cargo>> Listar()
+        {
+            List<Cargo> lstCargoRetorno = null;
+
+            try
+            {
+                DbCargo dbCargo = new DbCargo();
+                lstCargoRetorno = dbCargo.ListasrCargo();
+
+                if (lstCargoRetorno != null && lstCargoRetorno.Count > 0)
+                {
+                    return lstCargoRetorno;
+                }
+                else
+                {
+                    return lstCargoRetorno;
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Cargo> Carregar(int pCodCargo)
+        {
+            Cargo cargoRetrono = null;
+
+            if (pCodCargo <= 0)
+            {
+                throw new InvalidOperationException("Informe o código do cargo para carrega-lo.");
+            }
+
+            try
+            {
+                DbCargo dbCargo = new DbCargo();
+                cargoRetrono = dbCargo.CarregarCargo(pCodCargo);
+
+                if (cargoRetrono != null)
+                {
+                    return cargoRetrono;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Erro ao carregar cargo.");
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ Server/Controllers/Cadastro/CargoController.cs
@@ -0,0 +1,75 @@
+﻿using Controle.Cadastro;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Negocio.Cadastro;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Projeto_Contabilidade.Server.Controllers.Cadastro
+{
+    [Route("Cargo")]
+    [ApiController]
+    public class CargoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Carregar")]
+        public async Task<IActionResult> Carregar(int pCodCargo)
+        {
+            Cargo cargoRetorno = null;
+
+            try
+            {
+                ControleCargo controleCargo = new ControleCargo();
+                cargoRetorno = await controleCargo.Carregar(pCodCargo);
+                if (cargoRetorno != null)
+                {
+                    return Ok(cargoRetorno);
+                }
+                else
+                {
+                    return NotFound("Não foi possível carregar o cargo.");
+                }
+
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(new InvalidOperationException(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Exception(ex.Message));
+            }
+        }
+
+        [HttpGet]
+        [Route("Listar")]
+        public async Task<IActionResult> Listar()
+        {
+            List<Cargo> cargoRetorno = null;
+
+            try
+            {
+                ControleCargo controleCargo = new ControleCargo();
+                cargoRetorno = await controleCargo.Listar();
+                if (cargoRetorno != null)
+                {
+                    return Ok(cargoRetorno);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Não foi possível listar os cargos.");
+                }
+
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(new InvalidOperationException(ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Exception(ex.Message));
+            }
+        }
+    }
+}
