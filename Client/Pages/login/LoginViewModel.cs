bc8b518fbf7e2c18d87c92e61e608e0394bdf0aa
Lucas Cavalheiro <lucasribeiro120@hotmail.com>
2022-06-11 18:56:28 -0300

    Alterando carregamento de select CadastroEmprestimo, criando tela de Login e adicionando script para gravar cookie

---
+++ Client/Pages/login/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Projeto_Contabilidade.Client.Pages.login
+{
+    public class LoginViewModel
+    {
+
+        private string usuario { get; set; }
+        public string Usuario { get => usuario; set => usuario = value; }
+
+        private string password { get; set; }
+        public string Password { get => password; set => password = value; }
+    }
+}
